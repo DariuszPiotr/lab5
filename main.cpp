4ba75b4e6dd32b14263ec2b02fc9ce53748b943a
Dariusz Piotrowski <225969@student.pwr.edu.pl>
2016-05-21 16:06:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -51,7 +51,7 @@ int main(int argc, char const *argv[])
 				x=*(v.begin());
 				
 				v.erase(v.begin());
-				Sample testowy(x,v);
+				
 				
 				wines.push_back(new Sample(x,v));
 				v.clear();
